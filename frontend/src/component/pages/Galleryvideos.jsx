579f6b7d02add72e61f54ddc2bb1646f6a5b106f
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2026-01-22 10:58:40 +0530

    Add files via upload

---
+++ frontend/src/component/pages/Galleryvideos.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useTheme } from '../../Context/TheamContext';
+
+
+const Galleryvideos = () => {
+  const { theme } = useTheme();
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: theme === "dark" ? "#121212" : "#f5f5f5",
+        color: theme === "dark" ? "#ffffff" : "#000000",
+        fontSize: "24px",
+        fontWeight: "bold",
+      }}
+    >
+        
+      Coming Soon  Video Gallery🚀
+    </div>
+  );
+};
+
+ export default Galleryvideos
